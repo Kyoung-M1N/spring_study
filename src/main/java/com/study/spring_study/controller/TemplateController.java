b20d9807f126e8b2dcea1e34b49554c530ca7218
kymin <park48664866@gmail.com>
2023-12-03 15:43:48 +0900

    feat: 정적 컨텐츠와 템플릿 페이지 구현
    정적 컨텐츠 구현:
    - index.html파일을 이용한 welcome page 생성
    템플릿 페이지 구현:
    - template.html파일을 이용한 템플릿 페이지 생성
    - TemplateController를 이용한 템플릿 페이지 mapping

---
+++ src/main/java/com/study/spring_study/controller/TemplateController.java
@@ -0,0 +1,19 @@
+package com.study.spring_study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController {
+
+    @GetMapping(value = "template") // url에서 /다음에 들어갈 경로의 이름을 지정
+    public String templatePage(Model model) {
+        model.addAttribute("page", "template-page");
+        // path.html에서 attributeName을 찾아 model을 통해 값을 전달하여 attributeValue로 변경
+        return "template";
+        // resources/templates에 있는 html파일의 이름을 리턴하면 Thymeleaf의 viewResolver가 알아서 해당 파일을
+        // 찾아 리턴
+    }
+
+}
